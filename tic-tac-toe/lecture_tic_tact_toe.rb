6087cfba94200ef9d277ad6639d40d4d4b32ebcc
Steve Callear <scallear@outlook.com>
2014-12-03 20:33:35 +0000

    Created AI for 2 in a row and added it to the lecture_tic_tac_toe file

--- tic-tac-toe/lecture_tic_tact_toe.rb
+++ tic-tac-toe/lecture_tic_tact_toe.rb
@@ -1,26 +1,6 @@
-#1. Come up with requirements to determine the scope
-#2. Application Logic/flow
-#3. Translation of steps into code
-#4. Run to verify
-
-#Game Sudo code (step 2)
-
-#Draw a board
-
-#choose who's x and who's o
-
-#loop until winner/ no free spaces
-	#Player1 picks an empty square
-#	check win
-	#Player2 picks an empty square
-#	check win
-#end
-
-#if there is a winner
-#	create winning message
-#elsif tie 
-#	display cat message
 require 'pry'
+
+WINNING_LINES = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
 
 #Methods-------------------------------------------------------------
 
@@ -44,8 +24,8 @@ def empty_spaces(spaces)
 end
 
 def player_choice(spaces, side)
-		puts "Choose a space (1-9):"
-		choice = gets.chomp.to_i
+	puts "Choose a space (1-9):"
+	choice = gets.chomp.to_i
 	until empty_spaces(spaces).include?(choice)
 		puts "Space already taken, choose an empty space:"
 		choice = gets.chomp.to_i
@@ -53,72 +33,103 @@ def player_choice(spaces, side)
 	spaces[choice] = side
 end
 
+def check_two(spaces)
+    choices = nil
+    WINNING_LINES.each do |line|
+    	if spaces.values_at(*line).count("X") == 2
+    		choices = spaces.values_at(*line).select{|v| v.is_a? Numeric}.first
+    		break
+    	elsif spaces.values_at(*line).count("O") == 2
+    		choices = spaces.values_at(*line).select{|v| v.is_a? Numeric}.first
+            break
+    	end
+    end
+    return choices
+end
+
 def computer_choice(spaces, side)
-	choice = empty_spaces(spaces).sample
+	if check_two(spaces)
+		choice = check_two(spaces)
+	else
+		choice = empty_spaces(spaces).sample
+	end
 	spaces[choice] = side
 end
 
 def check_win(spaces)
-	winning_lines = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
-	winning_lines.each do |line| 
+	WINNING_LINES.each do |line| 
 		return "X" if spaces.values_at(*line).count("X") == 3
 		return "O" if spaces.values_at(*line).count("O") == 3
 	end
 	nil
 end
-#Game-----------------------------------------------------------
 
+def play_again
+	puts "Would you like to try again?"
+	gets.chomp.upcase
+end
+
+#Game-----------------------------------------------------------
 puts "Welcome to Tic-Tac-Toe!"
 
-#Choosing sides.................................................
+replay = "Y"
+
 begin
-	puts "Choose your side, X or O:"
-	player = gets.chomp.upcase
-end until player == "O" || player == "X"
-
-if player == "O"
-	computer = "X"
-	puts "You chose O's the computer will be X's. Press [enter] to continue..."
-	gets
-else
-	computer = "O"
-	puts "You chose X's the computer will be O's. Press [enter] to continue..."
-	gets
-end
-
-spaces = initialize_spaces
-
-draw_board(spaces)
-
-#The play...............................................................
-if player == "X"
+	#Choosing sides.................................................
 	begin
-		player_choice(spaces, player)
-		computer_choice(spaces, computer)
-		draw_board(spaces)
-		winner = check_win(spaces)
-	end until winner || empty_spaces(spaces).empty?
-else
-	begin
-		computer_choice(spaces, computer)
-		draw_board(spaces)
-		if empty_spaces(spaces).empty?
-			break
-		else
+		puts "Choose your side, X or O:"
+		player = gets.chomp.upcase
+	end until player == "O" || player == "X"
+	
+	if player == "O"
+		computer = "X"
+		puts "You chose O's the computer will be X's. Press [enter] to continue..."
+		gets
+	else
+		computer = "O"
+		puts "You chose X's the computer will be O's. Press [enter] to continue..."
+		gets
+	end
+	
+	spaces = initialize_spaces
+	
+	draw_board(spaces)
+	
+	#The play...............................................................
+	if player == "X"
+		begin
 			player_choice(spaces, player)
-		end
-		winner = check_win(spaces)
-	end until winner || empty_spaces(spaces).empty?
-end
+			winner = check_win(spaces)
+			break if winner || empty_spaces(spaces).empty?
+			computer_choice(spaces, computer)
+			draw_board(spaces)
+			winner = check_win(spaces)
+		end until winner || empty_spaces(spaces).empty?
+	else
+		begin
+			computer_choice(spaces, computer)
+			winner = check_win(spaces)
+			draw_board(spaces)
+			break if winner || empty_spaces(spaces).empty?
+			player_choice(spaces, player)
+			winner = check_win(spaces)
+		end until winner || empty_spaces(spaces).empty?
+	end
+	
+	#Win message...........................................................
+	if winner == player
+		puts "#{winner}'s won! Cograts, you beat the computer!"
+		replay = play_again
+	elsif winner == computer
+		puts "#{winner}'s won! Sorry, you lost...try again next time."
+		replay = play_again
+	else
+		puts "Cat game! It's a tie. Good game."
+		replay = play_again
+	end
 
-#Win message...........................................................
-if winner == player
-	puts "#{winner}'s won! Cograts, you beat the computer!"
-elsif winner == computer
-	puts "#{winner}'s won! Sorry, you lost...try again next time."
-else
-	puts "Cat game! It's a tie. Good game."
-end
+end until replay != "Y"
 
 
 
+#fin...........................
