30b8b025a7519ad1d8d4347767bedec18172e531
Steve Callear <scallear@outlook.com>
2014-12-17 14:54:40 +0000

    Starting the Black Jack project

---
+++ black-jack/black-jack.rb
@@ -0,0 +1,29 @@
+# create a deck
+require 'pry'
+
+deck = {:heart => ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'], :diamond => ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'], :spade => ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'], :club => ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']}
+# Infinite deck? or Shuffle?
+
+SUIT = [:heart, :diamond, :club, :spade]
+
+def init_deck
+    deck = {}
+    arr = []
+    (1..13).each {|value| arr[value-1] = value}
+    SUIT.each {|suit| deck[suit] = arr}
+    deck
+end
+
+    
+
+# check for 21
+# check for bust
+# check for check for 17
+# CHECK WIN
+
+puts "Welcome to black jack!\nLadies and Gentlemen place your bets!"
+
+binding.pry
+
+
+
