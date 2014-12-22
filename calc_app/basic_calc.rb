41615537ebc9224066e5432048b8f97ba3f3d536
Steve Callear <scallear@outlook.com>
2014-12-22 12:18:00 -0500

    updated basic_calc.rb based on discussion board feedback; also made final edits to rps_game.rb for final posting

--- calc_app/basic_calc.rb
+++ calc_app/basic_calc.rb
@@ -2,14 +2,14 @@ def say(msg)
   puts "\n=> #{msg}"
 end
 
-def check_num(num)
-  num =~ /\d/
+def check_number(number)
+  number =~ /\d/
 end
 
 def check_operator(operator)
   operator =~ /[+\-*\/]/
 end
-
+number
 system 'clear'
 
 puts "Welcome to my first Ruby calculator!\nLet's build an expression and solve it."
@@ -17,30 +17,30 @@ puts "Welcome to my first Ruby calculator!\nLet's build an expression and solve 
 begin
   begin
     say "What's the first number?"
-    num1 =  gets.chomp
-  end until check_num(num1)
+    number1 =  gets.chomp
+  end until check_number(number1)
 
   begin
     say "What's the second number?"
-    num2 =  gets.chomp
-  end until check_num(num2)
+    number2 =  gets.chomp
+  end until check_number(number2)
   
   begin
     say "What's the operator?\nEnter + for addition; - for subtraction; * for multiplication; / for division"
     operator = gets.chomp
   end until check_operator(operator)
 
-  if operator== "+"
-    results = num1.to_i + num2.to_i
+  if operator == "+"
+    results = number1.to_i + number2.to_i
   elsif operator == "-"
-    results = num1.to_i - num2.to_i
+    results = number1.to_i - number2.to_i
   elsif operator == "*"
-    results = num1.to_i * num2.to_i
+    results = number1.to_i * number2.to_i
   elsif operator == "/"
-    results = num1.to_f / num2.to_f
+    results = number1.to_f / number2.to_f
   end
   
-  say "#{num1} #{operator} #{num2} = #{results}"
+  say "#{number1} #{operator} #{number2} = #{results}"
 
   puts "Do you have another calculation?(Y/N)"
   answer = gets.chomp.upcase
--- rock_paper_scissors/rps_game.rb
+++ rock_paper_scissors/rps_game.rb
@@ -1,24 +1,33 @@
-print "Play Rock Paper Scissors!"
+CHOICES = { "s" => "scissors", "p" => "paper", "r" => "rock"}
+score = 0
 
-print "Choose one: (P/R/S)"
-player_choice = gets.chomp
+system 'clear'
 
-comp_choice = rand(3)
+puts "Welcome to Rock Paper Scissors!\nEach round you'll pick Rock (r), Paper (p), or Scissors (s).\n\n*Rock beats scissors*\n*Paper beats rock*\n*Scissors beats paper*\n\nYou'll earn 2 points for a win, 1 for a tie, and 0 for loss.\n\nGood Luck!"
 
-
-def choice_convert(choice)
-    if (choice == "0" || choice == "P")
-        choice == "paper"
-    elsif (choice == "1" || choice == "R")
-        choice == "rock"
-    elsif (choice == "2" || choice == "S")
-        choice = "scissors"
+begin
+    begin
+        puts "\nTo get started , pick one: (r, p, s)"
+        player_choice = gets.chomp.downcase
+    end until CHOICES.keys.include?(player_choice)
+    
+    computer_choice = CHOICES.keys.sample
+    
+    puts "You chose #{CHOICES[player_choice]} and the computer chose #{CHOICES[computer_choice]}..."
+    
+    if (player_choice == "s" && computer_choice == "p") || (player_choice == "r" && computer_choice == "s") ||(player_choice == "p" && computer_choice == "r")
+        puts "You win! :)"
+        score += 2
+    elsif player_choice == computer_choice
+        puts "You tied!"
+        score += 1
+    else
+        puts "The Computer wins! :("
     end
-end
+        
+    puts "Another round? (Y/N)"
+    answer = gets.chomp.upcase
+    
+end while answer == "Y"
 
-puts "Player choice is #{player_choice} and Comp choice is #{comp_choice}"
-
-puts choice_convert(player_choice)
-
-puts choice_convert(comp_choice)
-
+puts "Great job! You scored #{score} points!"
