eca9a92cb9d19b7667b6d03110fa139cdf193e00
Steve Callear <scallear@outlook.com>
2015-01-23 02:53:13 +0000

    update to work on other comp

--- black-jack/black_jack_lecture.rb
+++ black-jack/black_jack_lecture.rb
@@ -2,11 +2,7 @@ require 'pry'
 
 suit = ["♥", "♦", "♣", "♠" ]
 
-cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
-
-def play() 
-
-end
+cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
 def show_cards(player)
   cards = ""
@@ -16,20 +12,20 @@ end
 
 def total(player)
   total = 0
+  player.sort!
   player.each do |card|
     if (card[0] == "J" || card[0] == "Q" || card[0] == "K")
       total += 10
-    elsif card[0] == "A" && total >= 20
+    elsif card[0] == "A" && total >= 10
       total += 1
     elsif card[0] == "A"
       total += 11
     else
-      total += card[0]
+      total += card[0].to_i
     end
   end
   total
 end
-
 
 deck = cards.product(suit).shuffle
 
@@ -46,18 +42,23 @@ computer_cards << deck.pop
 my_cards << deck.pop
 computer_cards << deck.pop
 
-
 puts "The computer has the #{computer_cards[1][0]}#{computer_cards[1][1]} showing."
-puts "Your have #{show_cards(my_cards)}for a total of #{total(my_cards)}"
 
 begin
-  puts "Would you like to hit or stay? (hit/stay)"
-  hit_stay = gets.chomp.downcase
-end until hit_stay == "hit" || hit_stay == "stay"
+  puts "Your have #{show_cards(my_cards)}for a total of #{total(my_cards)}."
+  begin
+    puts "\nWould you like to hit or stay? (hit/stay)"
+    hit_stay = gets.chomp.downcase
+  end until hit_stay == "hit" || hit_stay == "stay"
+  if hit_stay == "hit"
+    my_cards << deck.pop
+  else
+    break
+  end
+end until total(my_cards) >= 21
 
-
-
-
+puts "#{total(my_cards)} #{show_cards(my_cards)}"
+puts "#{total(computer_cards)} #{show_cards(computer_cards)}"
 
 
 
