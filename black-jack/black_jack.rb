7a2a83441e56e10f1b65007a32641937fb923194
Steve Callear <scallear@outlook.com>
2015-01-24 00:31:39 +0000

    Minor fix.

--- black-jack/black_jack.rb
+++ black-jack/black_jack.rb
@@ -6,10 +6,16 @@ SUIT = ["♥", "♦", "♣", "♠" ]
 FACE = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
 #Methods
-def show_cards(player_cards)
+def show_user(player_cards, total)
   cards = ""
   player_cards.each {|card| cards += "#{card[0]}#{card[1]} "}
-  cards
+  puts "Your cards: #{cards}Total: #{total}"
+end
+
+def show_computer(player_cards, total)
+  cards = ""
+  player_cards.each {|card| cards += "#{card[0]}#{card[1]} "}
+  puts "Computer Cards: #{cards}Total: #{total}"
 end
 
 def total(player_cards)
@@ -47,20 +53,24 @@ end
 
 #Game Start
 puts "Welcome to balckjack! You've been given $25 to start, see how long you last. If you make it to $50, you win!"
-user_money = 25
+user_money = 20
 
 puts "What type of game would you like to play? Enter 1 for a single or 2 for a double deck?"
 deck_type = gets.chomp.to_i
 
 until deck_type == 1 || deck_type == 2
-  puts "Choose your game...enter 1 for single or 2 for double."
+  puts "Choose your game... enter 1 for single or 2 for double."
   deck_type = gets.chomp.to_i
 end
 
+#Round Start
 begin
   puts "New round! Ready? [Enter]"
   gets
+  
   system "clear"
+  
+  #Betting
   puts "You have $#{user_money}, place your bet! You may bet in increments of $5."
   user_bet = gets.chomp.to_i
   
@@ -86,14 +96,14 @@ begin
   user_cards << deck.pop
   computer_cards << deck.pop
   
-  puts "The computer has the #{computer_cards[1][0]}#{computer_cards[1][1]} showing."
+  puts "Computer Showing: #{computer_cards[1][0]}#{computer_cards[1][1]}"
   
   user_total = total(user_cards)
   computer_total = total(computer_cards)
   
   #Player Turn
   until user_total >= 21
-    puts "You have the #{show_cards(user_cards).strip}for a total of #{user_total}."
+    show_user(user_cards, user_total)
     begin
       puts "\nWould you like to hit or stay? (hit/stay)"
       hit_stay = gets.chomp.downcase
@@ -115,31 +125,34 @@ begin
   #Payout
   if user_total == 21
     if user_total == computer_total
-      puts "Your cards are #{show_cards(user_cards).strip}, for a total of #{user_total}."
-      puts "The computer's cards are #{show_cards(computer_cards).strip}, for a total of #{computer_total}."  
+      show_user(user_cards, user_total)
+      show_computer(computer_cards, computer_total) 
       puts "You tied..."
       user_money += payout(user_total, user_bet, deck_type, computer_total)
     else
-      puts "With the #{show_cards(user_cards).strip}, you got blackjack! You win!"
+      show_user(user_cards, user_total)
+      puts "You got blackjack! You win!"
       user_money += payout(user_total, user_bet, deck_type)
     end
   elsif user_total > 21
-    puts "Sorry, you lose. Your cards, #{show_cards(user_cards).strip}, equal #{user_total}, you busted!"
+    show_user(user_cards, user_total)
+    puts "You busted, you lose."
   elsif computer_total > 21
-    puts "Congrats, you win! The computer's cards, #{show_cards(computer_cards).strip}, equal #{computer_total}; the computer busted."
+    show_computer(computer_cards, computer_total)  
+    puts "The computer busted, you win!"
     user_money += payout(user_total, user_bet, deck_type)
   elsif computer_total == user_total
-    puts "Your cards are #{show_cards(user_cards).strip}, for a total of #{user_total}."
-    puts "The computer's cards are #{show_cards(computer_cards).strip}, for a total of #{computer_total}."  
+    show_user(user_cards, user_total)
+    show_computer(computer_cards, computer_total)
     puts "You tied..."
     user_money += payout(user_total, user_bet, deck_type, computer_total)
   elsif computer_total > user_total
-    puts "Your cards are #{show_cards(user_cards).strip}, for a total of #{user_total}."
-    puts "The computer's cards are #{show_cards(computer_cards).strip}, for a total of #{computer_total}."  
+    show_user(user_cards, user_total)
+    show_computer(computer_cards, computer_total)  
     puts "Sorry, you lose"
   elsif computer_total < user_total
-    puts "Your cards are #{show_cards(user_cards).strip}, for a total of #{user_total}."
-    puts "The computer's cards are #{show_cards(computer_cards).strip}, for a total of #{computer_total}."  
+    show_user(user_cards, user_total)
+    show_computer(computer_cards, computer_total)
     puts "Congrats, you win!"
     user_money += payout(user_total, user_bet, deck_type, computer_total)
   end
@@ -147,10 +160,11 @@ begin
   puts "You now have $#{user_money}."
   
 end until user_money < 5 || user_money >= 50
+#Round End
 
 if user_money >= 50
   puts "You beat the house! Congrats!"
 else
   puts "You're broke. Better luck next time..."
 end
-
+#Game End
