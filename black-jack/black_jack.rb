c3c9f082f24523f18c49b1b59d39d0526613fbd8
Steve Callear <scallear@outlook.com>
2015-01-24 21:45:04 +0000

    Fixed total to address edge cases for Aces. Ready to submit.

--- black-jack/black_jack.rb
+++ black-jack/black_jack.rb
@@ -20,19 +20,27 @@ end
 
 def total(player_cards)
   total = 0
-  player_cards.sort!
-  player_cards.each do |card|
+  
+  aces = player_cards.select {|card| card[0] == "A"}
+  other_cards = player_cards.select {|card| card[0] != "A"}
+
+  other_cards.each do |card|
     if (card[0] == "J" || card[0] == "Q" || card[0] == "K")
       total += 10
-    elsif card[0] == "A" && total >= 10
-      total += 1
-    elsif card[0] == "A"
-      total += 11
     else
       total += card[0].to_i
     end
   end
+  
+  aces.each do |card|
+    total += 11
+    total -= 10 if total > 21
+  end  
   total
+end
+
+def deal(player_cards, deck)
+  player_cards << deck.pop
 end
 
 def payout(player_total, bet, deck, computer_total=nil)
@@ -52,14 +60,14 @@ def payout(player_total, bet, deck, computer_total=nil)
 end
 
 #Game Start
-puts "Welcome to balckjack! You've been given $25 to start, see how long you last. If you make it to $50, you win!"
+puts "Welcome to balckjack! You've been given $20 to start, see how long you last. If you make it to $50, you win!"
 user_money = 20
 
-puts "What type of game would you like to play? Enter 1 for a single or 2 for a double deck?"
+puts "Enter 1 for a single deck or 2 for a double deck. Better odds for a double."
 deck_type = gets.chomp.to_i
 
 until deck_type == 1 || deck_type == 2
-  puts "Choose your game... enter 1 for single or 2 for double."
+  puts "You forgot to choose your deck... Enter 1 for a single or 2 for a double."
   deck_type = gets.chomp.to_i
 end
 
@@ -81,7 +89,8 @@ begin
   
   user_money -= user_bet
   
-  #Shuffle 
+  #Shuffle
+  puts "Shuffling..."
   deck = (FACE.product(SUIT) * deck_type).shuffle 
 
   #Deal
@@ -91,25 +100,25 @@ begin
   puts "Ready to deal? [Enter]"
   gets
   
-  user_cards << deck.pop
-  computer_cards << deck.pop
-  user_cards << deck.pop
-  computer_cards << deck.pop
-  
-  puts "Computer Showing: #{computer_cards[1][0]}#{computer_cards[1][1]}"
-  
+  deal(user_cards, deck)
+  deal(computer_cards, deck)
+  deal(user_cards, deck)
+  deal(computer_cards, deck)
+
   user_total = total(user_cards)
   computer_total = total(computer_cards)
+  
+  puts "Computer Showing: #{computer_cards[1][0]}#{computer_cards[1][1]}"
   
   #Player Turn
   until user_total >= 21
     show_user(user_cards, user_total)
     begin
-      puts "\nWould you like to hit or stay? (hit/stay)"
+      puts "\nWould you like to hit or stay? Enter [hit] or [stay]"
       hit_stay = gets.chomp.downcase
     end until hit_stay == "hit" || hit_stay == "stay"
     if hit_stay == "hit"
-      user_cards << deck.pop
+      deal(user_cards, deck)
       user_total = total(user_cards)
     else
       break
@@ -118,7 +127,7 @@ begin
   
   #Computer Turn
   until computer_total >= 17 || user_total > 21
-    computer_cards << deck.pop
+    deal(computer_cards, deck)
     computer_total = total(computer_cards)
   end
   
@@ -127,33 +136,33 @@ begin
     if user_total == computer_total
       show_user(user_cards, user_total)
       show_computer(computer_cards, computer_total) 
-      puts "You tied..."
+      puts "\nYou tied..."
       user_money += payout(user_total, user_bet, deck_type, computer_total)
     else
       show_user(user_cards, user_total)
-      puts "You got blackjack! You win!"
+      puts "\nYou got blackjack! You win!"
       user_money += payout(user_total, user_bet, deck_type)
     end
   elsif user_total > 21
     show_user(user_cards, user_total)
-    puts "You busted, you lose."
+    puts "\nYou busted, you lose."
   elsif computer_total > 21
     show_computer(computer_cards, computer_total)  
-    puts "The computer busted, you win!"
+    puts "\nThe computer busted, you win!"
     user_money += payout(user_total, user_bet, deck_type)
-  elsif computer_total == user_total
-    show_user(user_cards, user_total)
-    show_computer(computer_cards, computer_total)
-    puts "You tied..."
-    user_money += payout(user_total, user_bet, deck_type, computer_total)
   elsif computer_total > user_total
     show_user(user_cards, user_total)
     show_computer(computer_cards, computer_total)  
-    puts "Sorry, you lose"
+    puts "\nSorry, you lose."
   elsif computer_total < user_total
     show_user(user_cards, user_total)
     show_computer(computer_cards, computer_total)
-    puts "Congrats, you win!"
+    puts "\nCongrats, you win!"
+    user_money += payout(user_total, user_bet, deck_type, computer_total)
+  else
+    show_user(user_cards, user_total)
+    show_computer(computer_cards, computer_total)
+    puts "\nYou tied..."
     user_money += payout(user_total, user_bet, deck_type, computer_total)
   end
   
